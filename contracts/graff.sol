b77048feaadccb76b895669ee900c3d5c1638b6c
"Enok" <"enokslaboratory@gmail.com">
2022-06-05 11:13:59 -0700

    Comments Structure

--- contracts/graff.sol
+++ contracts/graff.sol
@@ -9,16 +9,27 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 /// @custom:security-contact enokslaboratory@gmail.com
 contract TheFreshestKids is ERC721, ERC721Enumerable, Pausable, Ownable {
-    using Counters for Counters.Counter;
 
+    //************************************//
+    //*******1. PROPERTY VARIABLES *******//
+    //************************************//
+    using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
+
+    //************************************//
+    //******* 2. LIFECYCLE METHODS *******//
+    //************************************//
     constructor() ERC721("The Freshest Kids", "TFK") {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "http://thefreshestkids.eth";
     }
 
+
+    //*************************************//
+    //******* 3. PAUSABLE FUNCTIONS *******//
+    //*************************************//
     function pause() public onlyOwner {
         _pause();
     }
@@ -27,12 +38,20 @@ contract TheFreshestKids is ERC721, ERC721Enumerable, Pausable, Ownable {
         _unpause();
     }
 
-    function safeMint(address to) public onlyOwner {
+
+    //************************************//
+    //******* 4. MINTING FUNCTIONS *******//
+    //************************************//
+    function safeMint(address to) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
+
+    //************************************//
+    //******** 5. OTHER FUNCTIONS ********//
+    //************************************//
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         whenNotPaused
@@ -41,8 +60,8 @@ contract TheFreshestKids is ERC721, ERC721Enumerable, Pausable, Ownable {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    // The following functions are overrides required by Solidity.
 
+    // The following functions are overrides required by Solidity.
     function supportsInterface(bytes4 interfaceId)
         public
         view
